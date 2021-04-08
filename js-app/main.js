b99227a745913f2485c4dedb8c95c050037a65c5
Rickie Le <rickienthefishies@gmail.com>
2021-04-08 14:03:28 -0500

    beans show up

--- js-app/main.js
+++ js-app/main.js
@@ -1,13 +1,30 @@
 const url = "https://localhost:5001/api/beanvariety/";
 
+const contentTarget = document.querySelector("#theBeans");
 const button = document.querySelector("#run-button");
+
 button.addEventListener("click", () => {
     getAllBeanVarieties()
         .then(beanVarieties => {
-            console.log(beanVarieties);
+            renderBeans(beanVarieties);
         })
 });
 
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
-}
+};
+
+const renderBeans = (beans) => {
+    const beansToStrings = beans.map((bean) => {
+        return `
+        <article class="criminal">
+        <h2>${bean.name}</h2>
+        <h2>${bean.region}</h2>
+        <h2>${bean.notes}</h2>
+        </article>
+        `
+    }).join("")
+
+    contentTarget.innerHTML = beansToStrings
+};
+
